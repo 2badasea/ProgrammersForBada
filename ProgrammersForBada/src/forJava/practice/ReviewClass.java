4b838bfdf26bd21fe225221557b3202b08035ef8
badalee <gnjqtpf1@gmail.com>
2023-04-05 10:37:14 +0900

    2023. 04. 05 10:37 commit

--- ProgrammersForBada/src/forJava/practice/ReviewClass.java
+++ ProgrammersForBada/src/forJava/practice/ReviewClass.java
@@ -1,39 +1,67 @@
 package forJava.practice;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class ReviewClass {
 
 	public static void main(String[] args) {
-		// 문자열 내 p와 y의 개수
-		// 대문자와 소문자가 섞여있는 문자열 s -> p의 개ㅜㅅ와 y의 개수가 같으면 true, 다르면 false를 리턴. 
-		// 대소문자 구분 x 
-		
-		String test1 = "pPoooyY";
-		String test2 = "Pyy";
-		
-		
 		ReviewClass rc = new ReviewClass();
-		System.out.println(rc.solution(test1)); 
-		System.out.println(rc.solution(test2)); 
+		
+		int test2 = 15;  // 23
+		System.out.println(rc.solution(test2));
+		System.out.println(rc.solution2(test2));
+		
+		// 2진수 연산을 해주는 식
+		System.out.println(rc.solution3(test2));
 	}
 	
-	private boolean solution(String s) {
-		int count = 0;
-		s = s.toLowerCase();
+	// 이진수로 변환해주는 함수 만들어보기
+	private String solution3(int test) {
 		
-		for(int i = 0; i<s.length(); i++) {
-			if( s.charAt(i) == 'p') {
-				count++;
-			}else if( s.charAt(i) == 'y') {
-				count--;
+		String result = "";
+		while(test > 0) {
+			result = (test % 2) + result; 
+			test = test/2;
+		}
+		return result;
+	}
+	
+	// Integer.toBinayString() 메서드 활용ㄴ
+	public int solution(int test) {
+		int before = cal(test);
+		int after = 0; 
+		
+		while(true) {
+			test++;
+			if( before == cal(test)) {
+				after = test;
+				break;
 			}
 		}
-		
-		return count == 0 ? true : false;
+		return test;
 	}
 	
+	private int cal(int test) {
+		String bit = Integer.toBinaryString(test);
+		int count = 0;
+		for(int i = 0; i<bit.length(); i++) {
+			if( bit.charAt(i) == '1') {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	// Integer.bitCount 활용
+	private int solution2(int test) {
+		int before = Integer.bitCount(test);
+		int after = 0;
+		while(true) {
+			test++;
+			if(before == Integer.bitCount(test)) {
+				after = before;
+				break;
+			}
+		}
+		return test;
+	}
 	
 }
