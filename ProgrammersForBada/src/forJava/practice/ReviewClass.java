44ff91fb2751ee72c1a8f4d387f46681fb03c331
badalee <gnjqtpf1@gmail.com>
2023-04-06 10:15:18 +0900

    2023. 04. 06 commit

--- ProgrammersForBada/src/forJava/practice/ReviewClass.java
+++ ProgrammersForBada/src/forJava/practice/ReviewClass.java
@@ -1,67 +1,82 @@
 package forJava.practice;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class ReviewClass {
 
 	public static void main(String[] args) {
 		ReviewClass rc = new ReviewClass();
+		// 첫 문자가 대문자, 그 외의 알파벳은 소문자인 문자열
+		// 첫 문자가 알파벳이 아닌 경우, 이어지는 알파벳은 소문자로. 
+		// 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수 완성하기
+		String test1 = "3people unFollowed me";
+		String test2 = "for the last week";
+		System.out.println(rc.jadenCase(test1));
+//		System.out.println(rc.jadenCase(test2));
 		
-		int test2 = 15;  // 23
-		System.out.println(rc.solution(test2));
-		System.out.println(rc.solution2(test2));
-		
-		// 2진수 연산을 해주는 식
-		System.out.println(rc.solution3(test2));
 	}
 	
-	// 이진수로 변환해주는 함수 만들어보기
-	private String solution3(int test) {
+	private String jadenCase(String s) {
+		String answer = "";
 		
-		String result = "";
-		while(test > 0) {
-			result = (test % 2) + result; 
-			test = test/2;
-		}
-		return result;
-	}
-	
-	// Integer.toBinayString() 메서드 활용ㄴ
-	public int solution(int test) {
-		int before = cal(test);
-		int after = 0; 
+		String[] strAry = s.split("");
+		boolean flag = true;
 		
-		while(true) {
-			test++;
-			if( before == cal(test)) {
-				after = test;
-				break;
+		for(String str : strAry) {
+			if(str.equals(" ")) {
+				flag = true;
+				answer += str;
+				continue; // continue => 다음 for문 조건으로 이동. 
 			}
+			
+			if(flag) {
+				str = str.toUpperCase();
+				flag = false;
+			}else {
+				str = str.toLowerCase();
+			}
+			answer += str;
 		}
-		return test;
+		return answer;
 	}
 	
-	private int cal(int test) {
-		String bit = Integer.toBinaryString(test);
+	private String maxAndMin(String s) {
+		// 1. 첫 번째 방식 => 배열의 형식으로 만든다음, Arrays.sort() 메서드 사용하기
+		String[] ary = s.split(" ");
+		int[] intAry = new int[ary.length];
 		int count = 0;
-		for(int i = 0; i<bit.length(); i++) {
-			if( bit.charAt(i) == '1') {
-				count++;
-			}
+		for(String n : ary) {
+			intAry[count++] = Integer.parseInt(n);
 		}
-		return count;
+		
+		Arrays.sort(intAry);
+		
+		String answer = intAry[0] + " " + intAry[intAry.length-1];
+		return answer;
 	}
 	
-	// Integer.bitCount 활용
-	private int solution2(int test) {
-		int before = Integer.bitCount(test);
-		int after = 0;
-		while(true) {
-			test++;
-			if(before == Integer.bitCount(test)) {
-				after = before;
-				break;
+	
+	private int useStack(String s) {
+		// stack을 이용해서 풀어보기
+		char[] ary = s.toCharArray();
+		Stack<Character> stack = new Stack<>();
+		
+		for(char c : ary) {
+			if(stack.size() > 0) {
+				if(stack.peek() == c) {
+					stack.pop();
+				} else {
+					stack.push(c);
+				}
+			} else {
+				stack.push(c);
 			}
 		}
-		return test;
+		
+		return ( stack.size() == 0) ? 1 : 0;
+		// return stack.isEmpty() ? 1 : 0  표현으로도 가능.
 	}
+	
 	
 }
